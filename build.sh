c4dd9b5f4712d23d496f3e0403f28eb6c52262d5
Niels Hoppe <mail@nielshoppe.de>
2017-02-18 21:28:31 +0100

    Move Jekyll sources to subdirectory

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-xsltproc -o about/index.html _xsl/cv.xsl _data/cv.rdf &&
+xsltproc -o site/about/index.html _xsl/cv.xsl _data/cv.rdf &&
 jekyll build --config _config.yml,_localconfig.yml --incremental
