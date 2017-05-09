0b60aff7e7abe4e3f5cf658a091e77ff4a1828a6
Niels Hoppe <mail@nielshoppe.de>
2017-05-09 18:48:18 +0200

    New project structure

--- build.sh
+++ build.sh
@@ -1,4 +1,1 @@
-cd site/_themes/my-website-theme && gulp && cd ../../.. &&
-cp -R site/_themes/my-website-theme/dist/assets/fonts site/assets/ &&
-xsltproc -o site/about/cv.html xsl/cv.xsl data/cv.rdf &&
 jekyll build --config _config.yml,_localconfig.yml --incremental
